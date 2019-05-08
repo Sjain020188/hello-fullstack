9a5d69ba6c25e061f385fcb9a745ef930d060528
Shruti Jain <shrutiitengg@gmail.com>
2019-05-08 11:28:24 +0900

    done

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{data}}</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+
+  data: () => ({
+    data: null
+  }),
+
+  mounted() {
+    fetch("/api")
+      .then(data => {
+        return data.text();
+      })
+      .then(x => (this.data = x));
+    // axios.get("/api").then(res => {
+    //   this.data = res.data;
+    // });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
